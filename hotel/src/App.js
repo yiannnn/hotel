089f7e4084bae789cfd64be6bde842bdaa74dcb8
yiannnn <yao20010210@gmail.com>
2022-10-10 18:10:55 +0800

    加分題一

--- hotel/src/App.js
+++ hotel/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState}from 'react'
 import  Container  from 'react-bootstrap/Container'
 // import Form from 'react-bootstrap/Form'
 import './App.css';
@@ -11,13 +11,50 @@ import { FaMapMarkerAlt } from 'react-icons/fa';
 import { IconContext } from "react-icons";
 import Badge from 'react-bootstrap/Badge';
 import {HotelData} from './data.js';
+// import Form from 'react-bootstrap/Form';
+import Select from 'react-select'
+
 
 const Login = () =>{
-    
+  var Result = HotelData;
+  var Origin = HotelData;
+  const [selected, setSelected] = useState();
+  const handleChange = event => {
+    console.log(event.target.value);
+    setSelected(event.target.value);
+  };
+  function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        /* next line works with strings and numbers, 
+         * and you may want to customize it to your needs
+         */
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+}
+if(selected === "score"){
+  Result = HotelData.sort(dynamicSort("HotelRewardScore"));
+}else if(selected === "price"){
+  Result = HotelData.sort(dynamicSort("HotelPrice"));
+}else if(selected === "default"){
+  console.log(Result)
+  //Result = Origin;
+}
+
     return (
-      <div className='p1'>
-        <h3>找到{HotelData.length}間住宿</h3>
-    {HotelData.map((data, index) => {
+  <div className='p1'>
+  <select value={selected} onChange={handleChange} className='DropDown' title='為您精選'>
+    <option key='default' value='default'hidden>為您推薦</option>
+    <option key='price' value='price'>價錢低到高</option>
+    <option key='score' value='score'>評分低到高</option>
+  </select>
+  <h3>找到{HotelData.length}間住宿</h3>
+    {Result.map((data, index) => {
         return (
      <Container className='border Container'>
       <img src={data.HotelImg} alt='' className='p3'/>
@@ -33,7 +70,7 @@ const Login = () =>{
             {data.HotelLocation}</p>
           </IconContext.Provider>
           <span className='p6'>{data.HotelNote}</span>
-          <h5 className='p8'>NT$:{data.HotelPrice}起</h5>
+          <h5 className='p8'>NT:${data.HotelPrice}起</h5>
         </div>
       </Container>  
       );
--- hotel/src/data.js
+++ hotel/src/data.js
@@ -1,41 +1,46 @@
 export var HotelData=[{
+    "HotelID":"1",
     "HotelName":"台北君悅飯店",
     "HotelLocation":"台北市",
     "HotelNote":"五星級的台北君悅酒店位於繁華的信義區，地理位置便利，毗鄰燈景絢爛的台北 101 大樓、台北世界貿易中心和國際會議中心，設有室外游泳池、SPA 養生會館、三溫暖、健身中心以及共 850 間精緻客房。",
     "HotelReward":"好極了",
-    "HotelRewardScore":"4.5",
-    "HotelPrice":"10,886",
+    "HotelRewardScore":4.5,
+    "HotelPrice":10886,
     "HotelImg":"https://pix8.agoda.net/hotelImages/736992/0/8f22372a0c1f5073f0c07e666002637c.jpg?ca=7&ce=1&s=600x"
    },{
+    "HotelID":"2",
      "HotelName":"裕太渡假館",
      "HotelLocation":"宜蘭縣",
      "HotelNote":"裕太渡假館設有共用休息室、露台以及附戶外休息區和 WiFi（免費）的空調住宿，距離罗东车站 6.4 公里。館內提供私人停車位。所有房型均鋪有磁磚地板，設有設備齊全且附微波爐的廚房、用餐區、平面電視，也有附淋浴設施和拖鞋的私人衛浴。住宿也提供冰箱、烤箱、迷你吧和電熱水壺。",
      "HotelReward":"非常好",
-     "HotelRewardScore":"4.3",
-     "HotelPrice":"2,516",
+     "HotelRewardScore":4.3,
+     "HotelPrice":2516,
      "HotelImg":"https://cf.bstatic.com/xdata/images/hotel/max1280x900/333710377.jpg?k=2ac2fc2b9537c6a3cd04aa87310a3274de8a718c2026ba862197ec00ca515aa6&o=&hp=1"
    },{
+     "HotelID":"3",
      "HotelName":"旅人驛站日暮微旅",
      "HotelLocation":"台東縣",
      "HotelNote":"旅人驛站日暮微旅位於台東市，距離鐵花村音樂聚落步行僅 5 分鐘，距離海濱公園 5 分鐘車程，距離台東機場 12 分鐘車程。開車前往台東車站約需 10～15 分鐘。這家旅館的每間客房均配備空調和平面電視。所有客房均附私人衛浴。為讓住客有更舒適的住宿體驗，也提供拖鞋和免費盥洗用品。",
      "HotelReward":"好極了",
-     "HotelRewardScore":"4.8",
-     "HotelPrice":"4,517",
+     "HotelRewardScore":4.8,
+     "HotelPrice":4517,
      "HotelImg":"https://cf.bstatic.com/xdata/images/hotel/max1280x900/318024678.jpg?k=aea32627adf87830c38f3f57ae6ff1b92f50316801895aee2ff09e48f13638a1&o=&hp=1"
    },{
+     "HotelID":"4",
      "HotelName":"有間寓背包旅宿",
      "HotelLocation":"澎湖縣",
      "HotelNote":"有間寓背包旅宿位在 Kuang-yang，提供 WiFi（免費）、空調、季節性室外游泳池和酒吧，距離林投公園 7.4 公里，距離國立澎湖科技大學 7.5 公里。部分房型提供附沖洗座、拖鞋、吹風機和免費盥洗用品的私人衛浴、提供露台和花園，每天供應亞洲風味早餐。",
      "HotelReward":"非常好",
-     "HotelRewardScore":"4.1",
-     "HotelPrice":"22,516",
+     "HotelRewardScore":4.1,
+     "HotelPrice":22516,
      "HotelImg":"https://cf.bstatic.com/xdata/images/hotel/max1280x900/366263477.jpg?k=f3bf1ca8b4551126a699898fc19e1771983b71f702719366e47fdceffcf8f968&o=&hp=1"
    },{
+     "HotelID":"5",
      "HotelName":"德立莊 - 高雄博愛館",
      "HotelLocation":"高雄市",
      "HotelNote":"德立莊 - 高雄博愛館距離六合觀光夜市 2 公里，距離國立科學工藝博物館有 2.2 公里。最近的機場是高雄國際機場，距離飯店 10 公里。每間客房都配有空調、平面有線電視、冰箱和保險箱。有些房間能欣賞到城市景觀。私人浴室配有淋浴設施、免費盥洗用品和吹風機。",
      "HotelReward":"非常好",
-     "HotelRewardScore":"4.3",
-     "HotelPrice":"5,201",
+     "HotelRewardScore":4.3,
+     "HotelPrice":5201,
      "HotelImg":"https://cf.bstatic.com/xdata/images/hotel/max1280x900/177167160.jpg?k=7328efdb5547b99f121932ee0053964e84640eae6d0fa6b1d6045c5d0775db95&o=&hp=1"
     }]
